8eaa988e86849635d6a4d35f91551eae81c3bb4e
vagrant <vagrant@localhost.localdomain>
2020-11-19 01:25:28 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # laravel8-crud-simple
+
 Laravel8 CRUD Simple
+
+```bash
+php artisan key:generate
+php artisan migrate
+php artisan make:seeder ArticlesTableSeeder
+php artisan route:list
+
+
+# for osx
+brew install jq
+
+# for linux
+sudo yum -y install jq
+sudo apt -y install jq
+
+curl http://192.168.33.20/api/articles/1 -s | jq
+```
---
+++ app/Http/Controllers/API/ArticleController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+
+        return $articles;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Article::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Article::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+        $article->update($request->all());
+
+        return $article;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+
+        return 204;
+    }
+}
